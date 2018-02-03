1b8ae76e2e20538ad8454922e3783df30cbf558d
mj002 <mohamadij2@gmail.com>
2018-02-03 15:58:37 +0330

    v9&v10

---
+++ v10/cmd/pascal/px/version.c
@@ -0,0 +1,17 @@
+/* Copyright (c) 1979 Regents of the University of California */
+
+static char sccsid[] = "@(#)version.c 1.2 3/6/81";
+
+    /*
+     *	this writes the declaration of the current time stamp
+     *	onto standard output.
+     *	useful for making Version.c to give the creation date for px.
+     */
+
+extern long time();
+
+main()
+{
+	printf( "long	createtime = %ld;\n" , time(0) );
+	return 0;
+}
